01ec8d71dae183406a4f3cc9027b99b216fa52c3
abcdaramgi <wwa1102@naver.com>
2023-03-26 01:15:00 +0900

    SpaceOdyssey

--- Assets/Script/GameSystem.cs
+++ Assets/Script/GameSystem.cs
@@ -6,10 +6,15 @@ public class GameSystem : MonoBehaviour
 {
     private PlayerData[] cList;
     public PlayerData ch;
+    public GameObject spaceStation;
+    public List<GameObject> spaceStations = new List<GameObject>();
+    public int[] wasdX = {80, -80, 0, 0};
+    public int[] wasdY = {0, 0, 80, -80};
     private int count;
     // Start is called before the first frame update
     void Start()
     {
+        initSpaceStation(7);
         
     }
 
@@ -23,6 +28,22 @@ public class GameSystem : MonoBehaviour
         // int who = Random.Range(0, count - 1);
         // cList[who].isTrue = false;
         ch.isTagger = true;
+    }
+
+    void initSpaceStation(int count){
+        for(int i = 0; i < count; i++){
+            GameObject newStation = Instantiate(spaceStation);
+            spaceStations.Add(newStation);
+            int j = i % 4;
+            int k = (i / 4) + 1;
+            // newStation.transform.position = new Vector3(0 + wasdX[j], 0 + wasdY[j], 0);
+            newStation.transform.position = new Vector3(0 + wasdX[j] * k, 0 + wasdY[j] * k, 0);
+            Debug.Log("i:" + i + "j:" + j);
+        }
+    }
+
+    void destroyStation(int index){
+        Destroy(spaceStations[index]);
     }
 
     void gameOver(){
