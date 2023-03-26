b1bac7ec4cdfce874bbc223f319d0f9d98c23cf1
abcdaramgi <86977236+abcdaramgi@users.noreply.github.com>
2023-03-26 22:58:47 +0900

    [7] UI Update

---
+++ Assets/Script/SliderSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderSystem : MonoBehaviour
+{
+    Slider slHP;
+    public PlayerData pd;
+    float fSliderBarTime;
+    void Start()
+    {
+       slHP = GetComponent<Slider>();
+    }
+ 
+ 
+    void Update()
+    {
+        if(gameObject.tag == "stamina"){
+            slHP.value = pd.Stamina * 0.01f;
+        } else if(gameObject.tag == "oxygen"){
+            slHP.value = pd.Oxygen * 0.01f;
+        }
+        
+        if (slHP.value <= 0)
+            transform.Find("Fill Area").gameObject.SetActive(false);
+        else
+            transform.Find("Fill Area").gameObject.SetActive(true);
+    }
+}
